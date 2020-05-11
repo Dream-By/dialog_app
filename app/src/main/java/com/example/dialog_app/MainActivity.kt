a1ccbce1cd4d59a76ce77b84f19636276479b4e9
Dream-By <dzmitry.dzianisau@gmail.com>
2020-05-11 12:35:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dialog_app/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dialog_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val myDialogFragment = MyDialogFragment()
+            val manager = supportFragmentManager
+            myDialogFragment.show(manager,"myDialog")
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dialog_app/MyDialogFragment.kt
@@ -0,0 +1,22 @@
+package com.example.dialog_app
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class MyDialogFragment : DialogFragment(){
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle("Важное сообщение!")
+                .setMessage("Покормите кота!")
+                .setIcon(R.drawable.hungrycat)
+                .setPositiveButton("ОК, иду на кухню") {
+                        dialog, id ->  dialog.cancel()
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
