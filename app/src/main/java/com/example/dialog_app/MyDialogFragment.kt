d33fe4f94588fbe06a0ed1be0c91d3475803e65d
Dream-By <dzmitry.dzianisau@gmail.com>
2020-05-11 12:44:01 +0300

    Add second-buttons dialog

--- app/src/main/java/com/example/dialog_app/MyDialogFragment.kt
+++ app/src/main/java/com/example/dialog_app/MyDialogFragment.kt
@@ -2,7 +2,9 @@ package com.example.dialog_app
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
 class MyDialogFragment : DialogFragment(){
@@ -10,12 +12,16 @@ class MyDialogFragment : DialogFragment(){
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setTitle("Важное сообщение!")
+            builder.setTitle("Выбор есть всегда!")
                 .setMessage("Покормите кота!")
-                .setIcon(R.drawable.hungrycat)
-                .setPositiveButton("ОК, иду на кухню") {
-                        dialog, id ->  dialog.cancel()
+                .setCancelable(true)
+                //.setIcon(R.drawable.hungrycat)
+                .setPositiveButton("Вкусная пища") {dialog, id ->
+                        Toast.makeText(activity,"Вы сделали правильный выбор",Toast.LENGTH_LONG).show()
                 }
+                .setNegativeButton("Здоровая Пища",DialogInterface.OnClickListener {dialog, id ->
+                    Toast.makeText(activity,"Возможно вы правы",Toast.LENGTH_LONG).show()
+                })
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
