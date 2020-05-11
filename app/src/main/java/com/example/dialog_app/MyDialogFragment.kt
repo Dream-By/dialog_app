8df924b8f1663d6ae2fa644a5ac2b10379dcdeb5
Dream-By <dzmitry.dzianisau@gmail.com>
2020-05-11 12:53:39 +0300

    Add example with setMultiChoiceItems

--- app/src/main/java/com/example/dialog_app/MyDialogFragment.kt
+++ app/src/main/java/com/example/dialog_app/MyDialogFragment.kt
@@ -4,26 +4,40 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
-class MyDialogFragment : DialogFragment(){
+class MyDialogFragment : DialogFragment() {
 
     private val catNames = arrayOf("Васька", "Рыжик", "Мурзик")
+    private val checkedItems = booleanArrayOf(false, true, false)
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setTitle("Выберите кота")
-                .setItems(catNames
-                ) { dialog, which ->
-                    Toast.makeText(activity, "Выбранный кот: ${catNames[which]}",
-                        Toast.LENGTH_SHORT).show()
+            builder.setTitle("Выберите котов")
+                .setMultiChoiceItems(catNames, checkedItems) {
+                        dialog, which, isChecked ->
+                    checkedItems[which] = isChecked
+                    val name = catNames[which] // Get the clicked item
+                    Toast.makeText(activity, name, Toast.LENGTH_LONG).show()
+                }
+                .setPositiveButton("Готово"
+                ) {
+                        dialog, id ->
+                    // User clicked OK, so save the selectedItems results somewhere
+                    for (i in catNames.indices) {
+                        val checked = checkedItems[i]
+                        if (checked) {
+                            Log.i("Dialog", catNames[i])
+                        }
+                    }
+                }
+                .setNegativeButton("Отмена") {
+                        dialog, _ ->  dialog.cancel()
                 }
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
-
-
-
 }
